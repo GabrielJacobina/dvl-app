46603b721d56495b03d9aa4ba9e3a202fc947e63
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-06-19 15:36:24 -0300

    GET /skills return sorted by name

--- src/main/java/com/dvlcube/app/rest/SkillService.java
+++ src/main/java/com/dvlcube/app/rest/SkillService.java
@@ -1,28 +1,20 @@
 package com.dvlcube.app.rest;
 
-import static com.dvlcube.app.manager.data.e.Menu.CONFIGURATION;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.dvlcube.app.interfaces.MenuItem;
 import com.dvlcube.app.jpa.repo.SkillRepository;
 import com.dvlcube.app.manager.data.SkillBean;
 import com.dvlcube.app.manager.data.vo.MxRestResponse;
 import com.dvlcube.utils.interfaces.rest.MxFilterableBeanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.dvlcube.app.manager.data.e.Menu.CONFIGURATION;
 
 /**
  * @since 4 de jun de 2019
@@ -39,7 +31,7 @@ public class SkillService implements MxFilterableBeanService<SkillBean, Long> {
 	@Override
 	@GetMapping
 	public Iterable<SkillBean> get(@RequestParam Map<String, String> params) {
-		return repo.firstPage();
+		return repo.firstPage(Sort.by(Sort.Direction.ASC, "name"));
 	}
 
 	@Override
